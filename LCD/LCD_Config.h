e7aef6ccd5f1ffe38da1c372c15f299809977305
Hashim Sobhi <99601907+Hashim-Sobhi@users.noreply.github.com>
2022-07-09 21:27:14 +0200

    Add files via upload

---
+++ LCD/LCD_Config.h
@@ -0,0 +1,61 @@
+/***************************************************************/
+/*  Author 	 : Hashim Sobhi                                 **/
+/*  Date    	 : 17.4.2021                                    **/
+/*  Description: LCD driver 	(configration file)	          **/
+/*  Version 	 : 1.0                                          **/
+/***************************************************************/
+
+#ifndef _LCD_CONFIG_H_
+#define _LCD_CONFIG_H_
+
+
+/*function set*/
+/*data length*/
+#define LCD_8bit ON
+#define LCD_4bit OFF
+/*set number of display lines*/
+#define LCD_2lines ON
+#define LCD_1lines OFF
+/*set character font*/
+#define LCD_5x8  ON
+#define LCD_5x10 OFF
+
+/*display control*/
+/*cursor dipaly*/
+#define CURSOR_DISPLAY  ON
+/*cursor blink*/
+#define CURSOR_BLINK  ON
+
+/*entry mode set*/
+/*cursor Increment*/
+#define CURSOR_INCREMENT  ON
+/*cursor decrement*/
+#define CURSOR_DECREMENT  OFF
+
+
+
+
+#define ON  1
+#define OFF 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
